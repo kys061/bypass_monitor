34587db8948c49566787266b1718c149b48c425c
yskang <kys061@gmail.com>
2018-02-22 17:59:25 +0900

    change time && bypass cmd

--- bypass_niagara_enable.sh
+++ bypass_niagara_enable.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 rl="$(runlevel | awk '{ print $2  }')"
-echo $(date -Iseconds) "runlevel is $rl" >> /var/log/stm_bypass.log
+echo "runlevel is $rl" | awk '{ print strftime(), $0; fflush()  }' >> /var/log/stm_bypass.log
 if [ "$rl" -eq 0  ]; then
-	    echo $(date -Iseconds) "runlevel is 0, forcing bypass" >> /var/log/stm_bypass.log
+	echo "runlevel is 0, forcing bypass" | awk '{ print strftime(), $0; fflush()  }' >> /var/log/stm_bypass.log
+			sudo niagara_util -d1
+			sudo niagara_util -r
 		    sudo niagara_util -r 0
 			sudo niagara_util -r 1
 else
 	if [ "$rl" -eq 6  ]; then
-		echo $(date -Iseconds) "runlevel is 6, forcing bypass" >> /var/log/stm_bypass.log
+		echo "runlevel is 6, forcing bypass" | awk '{ print strftime(), $0; fflush()  }' >> /var/log/stm_bypass.log
+		sudo niagara_util -d1
+		sudo niagara_util -r
 		sudo niagara_util -r 0
 		sudo niagara_util -r 1
 	fi
 fi
-echo $(date -Iseconds) "=== Completed bypass_niagara_enable.sh" >> /var/log/stm_bypass.log
+echo "=== Completed bypass_niagara_enable.sh" | awk '{ print strftime(), $0; fflush()  }' >> /var/log/stm_bypass.log
